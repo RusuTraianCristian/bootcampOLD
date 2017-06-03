569b97ea21ddfeb04a70f138a2e8be649869f685
Rusu Traian Cristian <RusuTraianCristian@users.noreply.github.com>
2017-06-03 10:24:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+$(function() {
+
+	var getInfo = function(callback) {
+		Twitch.api({method: 'channel'}, function(error, channel) {
+		  callback(channel);
+		});
+	}
+
+	 Twitch.init({clientId: 'eg9h1ip6k85n8slq0ia88deew5af8ir'}, function(error, status) {
+
+	 	console.log(status);
+
+	 	if (status.authenticated) {
+	 		$('.twitch-connect').hide();
+			$('#header').css("display", "block");
+			$('#footer').css("display", "block");
+			$('#menu').css("display", "block");
+			$('.hiddeniflogged').css("display", "none");
+	 		getInfo(function(data) {
+				// checks if logged in to show specified information
+				$('#picture').attr('src',data.logo);
+				$('.headerphoto').attr('src',data.logo);
+	 			$('#visit').text(data.display_name).attr('href',data.url);
+				$('#username').text(data.display_name);
+				$('.partner').text("Partner:" + " " + data.partner);
+	 		});
+	 	} else {
+			// hides all user information IF NOT logged in
+	 		$('#login-info').hide();
+			$('body').css("background", "#0d0d11");
+	 	}
+
+	 });
+
+	 var login = function()
+	 {
+	 	Twitch.login({
+			// reads API info from Twitch if logged in
+		  scope: ['user_read', 'channel_read', 'channel_subscriptions']
+		});
+	 }
+
+	 var logout = function() {
+	     Twitch.logout(function(error) {
+	         // redirects to root URL when logging Off
+			 window.location = "http://twitchbootcamp.com";
+			 // shows twitch connect button if logged out
+		     $('.twitch-connect').show();
+		     $('strong').text('');
+		     $('#picture').attr('src','');
+		     $('#visit').text('').attr('href','#');
+             // hides everything else if logged out
+		     $('#login-info').hide();
+		 });
+	 }
+
+	 $('.twitch-connect').click(function(e) {
+	 	e.preventDefault();
+
+	 	login();
+	 })
+
+	 $('.twitch-disconnect').click(function(e) {
+	 	e.preventDefault();
+
+	 	logout();
+	 })
+
+})
